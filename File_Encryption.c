a12e4ece90c213f068b3597977d08c7f69f4c040
saadfahmitest <s.fahmi2618@uca.ac.ma>
2025-01-01 23:52:28 +0100

    c

---
+++ File_Encryption.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int get_bitmask(char *key)
+{
+        int hash = 0;
+        while(*key)
+        hash+= (*key++);
+        return(hash);
+
+}
+
+int encrypt(int c, char* key)
+{
+    int bitmask = get_bitmask(key);
+    return c ^ bitmask;
+
+
+}
+int main(int arc, char* argv[])
+{
+
+            int c;
+            while((c = getchar()) != EOF)
+            {
+                    putchar(encrypt(c, argv[1]));
+
+            }
+}
